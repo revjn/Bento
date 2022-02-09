29e49925885338e285ea0e86a835dff0dc1e739d
revjn <99330020+revjn@users.noreply.github.com>
2022-02-09 19:38:59 +0000

    Update config.js

--- config.js
+++ config.js
@@ -53,7 +53,7 @@ const CONFIG = {
       id: '1',
       name: 'Github',
       icon: 'github',
-      link: 'https://github.com/',
+      link: 'https://github.com/revjn',
     },
     {
       id: '2',
